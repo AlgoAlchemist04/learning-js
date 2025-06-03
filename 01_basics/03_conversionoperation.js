93d2cbd5e93de5ad14bee991400ecdaa326aa6ce
ANIKET <149222310+AlgoAlchemist04@users.noreply.github.com>
2025-06-03 16:12:34 +0000

    type conversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,26 @@
+let score=69
+let score1=null
+let score2="123abc"
+let score3=false
+//console.log(typeof score)
+//console.log(typeof score1)
+
+
+let valueinNumber=Number(score1)
+let valueinNumber2=Number(score2)
+let valueinNumber3=Number(score3)
+console.log(valueinNumber)
+console.log(typeof valueinNumber)
+
+console.log(typeof valueinNumber2)
+
+console.log(valueinNumber3)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1  false=> 0
+// ""=> false
+// "aniket"=> true
+
+let isLoggedIn=0;
+console.log(Boolean(isLoggedIn))
