331fe9729c24ff0df388850d528217051966347e
ANIKET <149222310+AlgoAlchemist04@users.noreply.github.com>
2025-06-03 17:19:14 +0000

    summary of datatypes primitive and non-primitive

---
+++ 01_basics/05_datatypes.js
@@ -0,0 +1,26 @@
+// primitive datatypes
+
+
+//7 types: String, number, boolean, null, undefined, Symbol, BigInt
+
+
+//Reference (non-primitive)
+
+//Arrays, Objects, Functions
+
+const heros=["Ironman", "Spider-man", "Dr.strange", "Black-panther"]
+console.log(heros)
+
+const myObj={
+name:"Aniket",
+age:21,
+isMarried:false
+
+}
+console.log(myObj)
+
+const myFun=function(){
+console.log("hello Aniket")
+}
+myFun()
+console.log(typeof myFun)
