0097c6f52e239c254ec4f6a0d96ffad2b69740fa
ANIKET <149222310+AlgoAlchemist04@users.noreply.github.com>
2025-06-03 12:58:04 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat code to js new version
+//alert(2+3) will work in browser not in nodejs
+
+let name="aniket"
+let age=21
+let loggedIn=true
+
+console.table([name,age,loggedIn])
+
+/*
+number
+bigInt
+string=> ""
+boolean =>true/false
+null => standalone value
+undefined=> value is not assigned
+symbol=> adds uniqueness
+*/
+
+console.log(typeof(undefined))
+console.log(typeof null)
