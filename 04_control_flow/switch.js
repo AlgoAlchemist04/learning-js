628777cc3319b5f564912f297b420ab762449472
ANIKET <149222310+AlgoAlchemist04@users.noreply.github.com>
2025-06-07 20:47:55 +0000

    learn switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,19 @@
+const month=3;
+
+switch(month){
+case 1:
+    console.log("January");
+    break;
+case 2:
+    console.log("Feb");
+    break;
+case 3:
+    console.log("Mar");
+    //break;  //if we remove this break the code below it will execute except default case
+case 4:
+    console.log("Apr");
+    break;
+default:
+    console.log("enter valid month less than or equal to apr i.e 4")
+    break;
+}
