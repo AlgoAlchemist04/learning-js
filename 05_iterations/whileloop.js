c1548c60bd50f7550d2b05d5e882b821c7c294fe
ANIKET <149222310+AlgoAlchemist04@users.noreply.github.com>
2025-06-09 18:08:59 +0000

    learn while and do while

---
+++ 05_iterations/whileloop.js
@@ -0,0 +1,22 @@
+//while 
+
+let i=0
+while(i<=5){
+console.log(i)
+i++
+}
+
+let j=0
+do{    //this will execute once irrespective of condition
+console.log(j)
+j++
+}
+while(j<0)
+
+
+    let myArr=["superman", "man_of_steel","kal_el", "clark_kent"]
+let index=0;
+    while(index<myArr.length){
+console.log(myArr[index])
+index++;
+    }
